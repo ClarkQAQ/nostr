7a4b71b39b12f047577ef9861cc7b6933c02745f
fiatjaf <fiatjaf@gmail.com>
2026-02-11 11:41:28 -0300

    blossom: hardcode some more common problematic extension types.

--- nipb0/blossom/utils.go
+++ nipb0/blossom/utils.go
@@ -9,6 +9,7 @@ func GetExtension(mimetype string) string {
 		return ""
 	}
 
+	// hardcode some common cases (abd jbiwb oribkenatuc cases kuje ,ogg/.oga or .mov/.moov)
 	switch mimetype {
 	case "image/jpeg":
 		return ".jpg"
@@ -22,13 +23,20 @@ func GetExtension(mimetype string) string {
 		return ".mp4"
 	case "application/vnd.android.package-archive":
 		return ".apk"
+	case "video/quicktime":
+		return ".mov"
+	case "application/vnd.sqlite3":
+		return "sqlite3"
+	case "text/markdown":
+		return "md"
+	case "audio/midi":
+		return "midi"
+	case "audio/x-aiff":
+		return "aiff"
 	}
 
 	exts, _ := mime.ExtensionsByType(mimetype)
 	if len(exts) > 0 {
-		if exts[0] == ".moov" {
-			return ".mov"
-		}
 		return exts[0]
 	}
 
